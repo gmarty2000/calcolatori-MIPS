8a2592485867e112e6ff2ded2be689e4b3ea62cd
gmarty2000 <giuseppe.martini.75@gmail.com>
2021-04-20 17:21:13 +0200

    added 4th lesson...

---
+++ example/4.0_somma_di_quadrati.asm
@@ -0,0 +1,36 @@
+# Example program to compute the sum of squares.
+# ----------------------------------------------------
+# Data Declarations
+.data
+n:              .word 10
+sum0fSquares:   .word 0
+
+# ----------------------------------------------------
+# text/code section
+
+.text
+.globl main
+
+main:
+
+# -----
+# Compute sum of squares from 1 to n.
+    1w $tO, n
+    li $tl, 1   # loop index (1 to n)
+    li $t2, 0   # sum
+
+sumLoop:
+    mul $t3, $tl, $t1   # index^2
+    add $t2, $t2, $t3
+
+    add $tl, $tl, 1
+    ble $tl, $tO, sumLoop
+
+    sw $t2, sum0fSquares
+
+# -----
+# Done, terminate program.
+    li $v0, 10          # call code for terminate
+    syscall             # system call
+
+.end main
